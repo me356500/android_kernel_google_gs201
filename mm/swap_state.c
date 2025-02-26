505ee655016bf72a37a977f588c255ca37d3f19f
wyc_home_PC <jlxuugu2@gmail.com>
2025-02-26 10:19:13 +0800

    fix/record app swap in
    Change-Id: Iadab8f6220e214f4f718bf3447fab1a2040862fc

--- mm/swap_state.c
+++ mm/swap_state.c
@@ -495,8 +495,9 @@ struct page *lookup_swap_cache(swp_entry_t entry, struct vm_area_struct *vma, un
 		// else
 		// 	count_vm_event(SWPIN_ZRAM);
 
-		if (print_log)
+		if (!page && print_log) { // majfault
 			put_app_swap_in_pattern(page_uid, si_type);
+		}
 		if (swp_type(entry)) // mark: temp to count page fault in zram,swp
 			count_vm_event(THP_SWPOUT_FALLBACK); // page fault on zram
 		else
