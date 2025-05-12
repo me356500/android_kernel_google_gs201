e83375546983f0f3b8675affc68ca9172edce7b0
wyc_home_PC <jlxuugu2@gmail.com>
2025-05-12 11:13:42 +0800

    fix/switch_majfault for 'flash' only
    Change-Id: Iae4d6e670f0c8449c4838792f73014598030bc85

--- mm/memory.c
+++ mm/memory.c
@@ -3719,7 +3719,7 @@ vm_fault_t do_swap_page(struct vm_fault *vmf)
 		ret = VM_FAULT_MAJOR;
 		count_vm_event(PGMAJFAULT);
 		// or adj <= 100 
-		if (vma && vma->vm_mm && vma->vm_mm->owner && vma->vm_mm->owner->signal && vma->vm_mm->owner->signal->oom_score_adj == 0
+		if (swp_type(entry) == 1 && vma && vma->vm_mm && vma->vm_mm->owner && vma->vm_mm->owner->signal && vma->vm_mm->owner->signal->oom_score_adj == 0
 			&& signal_app_switch) {
 			count_vm_event(USER_APP_MAJFAULT);
 		}
