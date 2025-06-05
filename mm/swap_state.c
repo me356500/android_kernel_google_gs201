91505c7e57fd5645be7fdbb690cb020f07b72b97
wyc_home_PC <jlxuugu2@gmail.com>
2025-06-05 13:25:26 +0800

    typo
    Change-Id: Ieb579e7cd2ac9dd60c244741a7a6cb496ae1cd43

--- mm/swap_state.c
+++ mm/swap_state.c
@@ -1005,14 +1005,14 @@ struct page *swap_cluster_readahead(swp_entry_t entry, gfp_t gfp_mask, struct vm
 		else {
 			mask = get_app_ra_window(page_uid, page_pid, 0) - 1;
 		}
-		if (mask != 0)
-			mask = prefetch_window_size - 1;
+		// if (mask != 0)
+		//	mask = prefetch_window_size - 1;
 	}
 	// wyc: overflow_prefetch
 	if (per_app_vma_prefetch) {
 		same_vma_window = get_app_same_vma_window(page_uid, page_pid);
-		if (same_vma_window != 1)
-			same_vma_window = prefetch_window_size;
+		// if (same_vma_window != 1)
+		//	same_vma_window = prefetch_window_size;
 	}
 	// overflow lookahead limit (default 16)
 	if (overflow_fixed_window) {
